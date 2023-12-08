ad02a5a5718c259a1cdec5ae94160d06d1b1ae62
Rafał Jopek <rafaljopek@hotmail.com>
2023-12-08 17:24:20 +0100

    Update menu.c

--- tests/menu.c
+++ tests/menu.c
@@ -4,11 +4,19 @@
 
 #include "glbdf.h"
 
-static void lastMenuItemClicked( MenuItem *item )
+void onExitClicked( MenuItem *pMenuItem )
 {
-   if( item )
+   if( pMenuItem && pMenuItem->pMenu )
    {
-      PrintMenuItemStruct( item );
+      ExitApp( pMenuItem->pMenu->pApp );
+   }
+}
+
+static void lastMenuItemClicked( MenuItem *pMenuItem )
+{
+   if( pMenuItem )
+   {
+      PrintMenuItemStruct( pMenuItem );
    }
 }
 
@@ -27,7 +35,7 @@ int main()
       MenuBarAddItem( fileMenu, "Print",            GLFW_KEY_P, GLFW_MOD_CONTROL, lastMenuItemClicked );
       MenuBarAddItem( fileMenu, "Printer setup...", 0, 0,                         lastMenuItemClicked );
       MenuBarAddItem( fileMenu, "DOS Shell",        0, 0,                         lastMenuItemClicked );
-      MenuBarAddItem( fileMenu, "Exit",             GLFW_KEY_Q, GLFW_MOD_CONTROL, lastMenuItemClicked );
+      MenuBarAddItem( fileMenu, "Exit",             GLFW_KEY_Q, GLFW_MOD_CONTROL, onExitClicked );
 
    MenuBar *editMenu = MenuBarNew( menu, "Edit" );
       MenuBarAddItem( editMenu, "Undo",           GLFW_KEY_Z, GLFW_MOD_CONTROL, lastMenuItemClicked );
